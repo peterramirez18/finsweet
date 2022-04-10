81a72e676eb57e2085f338f3b974a0af723abc2b
peterramirez18 <peterramirez1812@gmail.com>
2022-04-10 11:50:54 -0400

    add footer and are you ready section

---
+++ src/app/components/are-you-ready/are-you-ready.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-are-you-ready',
+  templateUrl: './are-you-ready.component.html',
+  styleUrls: ['./are-you-ready.component.scss']
+})
+export class AreYouReadyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
